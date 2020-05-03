f073966deae504bbadff4256113e9396356d17f1
sabdeolive <60741615+sabdeolive@users.noreply.github.com>
2020-05-03 14:51:22 +0200

    Update Metagenomic analysis R code (using class object and subject data with file name column).R

--- Metagenomic analysis/Metagenomic analysis R code (using class object and subject data with file name column).R
+++ Metagenomic analysis/Metagenomic analysis R code (using class object and subject data with file name column).R
@@ -1,3 +1,19 @@
+#Install Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.10")
+
+#Install phyloseq
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("phyloseq")
+
+# Check if BiocManager is installed
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+# Install HMP2Data package using BiocManager
+BiocManager::install("HMP2Data")
+
 library(BiocManager)
 library(HMP2Data)
 library(phyloseq)
@@ -83,4 +99,162 @@ T2D.rm.gut
 # tax_table()   Taxonomy Table:    [ 12062 taxa by 7 taxonomic ranks ]
 View(T2D.rm.gut)
 View(T2D.rm.gut@sam_data) # 50 samples = correct. 
+# NOTE: Final phyloseq to work with (prior to taxa filtering) = T2D.rm.gut
+
+write.table(T2D.rm.gut@otu_table@.Data, file="c:/Users/sabde/Documents/T2D ps w filtered subjects otu_table.txt", sep="\t", row.names = TRUE, col.names = NA)
+# This file was used to compare the sample IDs to those in the metabolome data file Sample IDs in the metabolome data that did not match those in the phyloseq object were removed from the metabolome data file.
+
+#### Removal of all samples from the T2D.rm.gut that are not shared between the T2D.rm.gut and the metabolome data
+# NOTE: T2D.rm.gut has 869 samples and the metabolome data has 555 (after removing samples from the metabolome data that did not match the sample IDs in the phyloseq)
+### Load metabolome data
+metabolomics <- `metabolome_abundance.(excl..all.samples.not.in.phyloseq).7.2`
+
+### Make a vector of the sample IDs in the metabolomics data file 
+sampleIDs.met.vec <- as.vector(metabolomics[,1])
+sampleIDs.met.vec
+length(sampleIDs.met.vec) # 555 samples = correct
+
+### Remove all the samples that are not in this vector from T2D.rm.gut using prune_samples function (only keeps those that have been defined by e.g. vector) 
+T2D.rm.gut
+# phyloseq-class experiment-level object
+# otu_table()   OTU Table:         [ 12062 taxa and 869 samples ]
+# sample_data() Sample Data:       [ 869 samples by 22 sample variables ]
+# tax_table()   Taxonomy Table:    [ 12062 taxa by 7 taxonomic ranks ]
+T2D.F.sub_sam <- prune_samples(sampleIDs.met.vec, T2D.rm.gut)
+T2D.F.sub_sam
+# phyloseq-class experiment-level object
+# otu_table()   OTU Table:         [ 12062 taxa and 555 samples ]
+# sample_data() Sample Data:       [ 555 samples by 22 sample variables ]
+# tax_table()   Taxonomy Table:    [ 12062 taxa by 7 taxonomic ranks ]
+
+
+#### Filter NA values
+table(tax_table(T2D.F.sub_sam)[,"Family"],exclude = NULL) #1650 NA
+T2D.fil <- subset_taxa(T2D.F.sub_sam, !is.na(Family)) 
+T2D.fil #12062 taxa - 1650 = 10412 taxa
+
+table(tax_table(T2D.F.sub_sam)[,"Genus"], exclude = NULL) #5953 NA (went up from 4303 to 5953 after merging and filtering of samples+subjects?)
+T2D.fil <- subset_taxa(T2D.F.sub_sam, !is.na(Genus)) 
+T2D.fil #10412 taxa -> 6109 taxa
+
+##Filtering by species = not necessary 
+# table(tax_table(T2D.F.sub_sam) [,"Species"], exclude = NULL) #4824 NA
+# T2D.fil <- subset_taxa(T2D.F.sub_sam, !is.na(Species))
+# T2D.fil #6109 taxa -> 1285 taxa
+
+#### Subset the phyloseq class object into classification
+IR_ps.fil <- subset_samples(T2D.fil, as_subject_ID == IR_v)
+IR_ps.fil # T2D = 2208 samples, IR_ps.fil = 21 samples (CHECK).
+# phyloseq-class experiment-level object
+# otu_table()   OTU Table:         [ 1281 taxa and 21 samples ]
+# sample_data() Sample Data:       [ 21 samples by 13 sample variables ]
+# tax_table()   Taxonomy Table:    [ 1281 taxa by 7 taxonomic ranks ]
+
+
+IS_ps.fil <- subset_samples(T2D.fil, as_subject_ID == IS_v)
+IS_ps.fil # T2D = 2208 samples, IS_ps.fil = 36 samples (CHECK).
+# phyloseq-class experiment-level object
+# otu_table()   OTU Table:         [ 1281 taxa and 36 samples ]
+# sample_data() Sample Data:       [ 36 samples by 13 sample variables ]
+# tax_table()   Taxonomy Table:    [ 1281 taxa by 7 taxonomic ranks ]
+
+
+#### Prevalence filter and plotting prevalence for each classification (FINSH)
+### Subset taxa (done before, see 'Subset the phyloseq class object into classification using IR_v and IS_v')
+names.OTU <- taxa_names(T2D.fil)
+
+## IR
+keep.IR.taxa <- names.OTU[rowSums(IR_ps.fil@otu_table)>0] #makes a character vector of all the taxa to keep (i.e. all those present in at least 1 sample) in the phyloseq for IR
+IR_ps.fil #1281 taxa 
+IR_ps.fil <- prune_taxa(keep.IR.taxa, IR_ps.fil)
+IR_ps.fil #415 taxa
+
+## IS
+keep.IS.taxa <- names.OTU[rowSums(IS_ps.fil@otu_table)>0]
+IS_ps.fil #1281 taxa
+IS_ps.fil <- prune_taxa(keep.IS.taxa, IS_ps.fil)
+IS_ps.fil #490 taxa
+
+### Prevalence filter IR (filtering of taxa)
+##Subset the remaining phyla 
+prevalence.df.IR <- subset(prevalence.df, Phylum %in% get_taxa_unique(IR_ps.fil, "Phylum")) #get_taxa_uniqe is used to determine the different taxa present for a particular taxonomic rank in a given phyloseq-class object
+## Visualize prevalence and total read count in order to determine prevalence threshold
+ggplot(prevalence.df.IR, aes(TotalAbundance, Prevalence / nsamples(IR_ps.fil),color=Phylum)) + 
+  geom_hline(yintercept = 0.05, alpha = 0.5, linetype = 2) + geom_point(size = 2, alpha = 0.7) + 
+  scale_x_log10() +  xlab("Total Abundance") + ylab("Prevalence [Frac. Samples]") + facet_wrap(~Phylum) + 
+  theme(legend.position="none")
+## Define prevalence threshold as 5% of total samples (CHECK: chose 0.05 as it seems there is a slight natural separation at around this prevalence in the actinobacteria and proteobacteria taxa)
+prevalenceThreshold.IR <- 0.05*nsamples(IR_ps.fil) #i.e. taxa have to appear in a minimum of 0.5% of samples or they will be removed
+prevalenceThreshold.IR # 1.05 (is this the cut-off point for the prevalence in the graphs?)
+
+### Execute this prevalence filter using prune_taxa() function (i.e. keeps rows/taxa where prevalence >= 0.05 in IR group)
+keepTaxa.IR <- rownames(prevalence.df.IR)[(prevalence.df.IR$Prevalence >= prevalenceThreshold.IR)]
+IR_ps.fil #415 taxa
+IR_ps.fil <- prune_taxa(keepTaxa.IR,IR_ps.fil) 
+IR_ps.fil #415 taxa -> 413 taxa
+
+
+### Prevalence filter IS
+##Subset the remaining phyla 
+prevalence.df.IS <- subset(prevalence.df, Phylum %in% get_taxa_unique(IS_ps.fil, "Phylum")) #get_taxa_uniqe is used to determine the different taxa present for a particular taxonomic rank in a given phyloseq-class object
+## Visualize prevalence and total read count in order to determine prevalence threshold
+ggplot(prevalence.df.IS, aes(TotalAbundance, Prevalence / nsamples(IS_ps.fil),color=Phylum)) + 
+  geom_hline(yintercept = 0.05, alpha = 0.5, linetype = 2) + geom_point(size = 2, alpha = 0.7) + 
+  scale_x_log10() +  xlab("Total Abundance") + ylab("Prevalence [Frac. Samples]") + facet_wrap(~Phylum) + 
+  theme(legend.position="none")
+## Define prevalence threshold as 5% of total samples (CHECK: chose 0.05 as it seems there is a slight natural separation at around this prevalence in the actinobacteria and proteobacteria taxa)
+prevalenceThreshold.IS <- 0.05*nsamples(IS_ps.fil) #i.e. taxa have to appear in a minimum of 0.5% of samples or they will be removed
+prevalenceThreshold.IS # 1.8 (is this the cut-off point for the prevalence in the graphs?)
+
+### Execute this prevalence filter using prune_taxa() function (i.e. keeps rows/taxa where prevalence >= 0.05 in IR group)
+keepTaxa.IS <- rownames(prevalence.df.IS)[(prevalence.df.IS$Prevalence >= prevalenceThreshold.IS)]
+IS_ps.fil #490 taxa
+IS_ps.fil <- prune_taxa(keepTaxa.IS,IS_ps.fil) 
+IS_ps.fil #490 taxa -> 489 taxa
+
+
+### Filter taxa of the whole T2D.rm.gut phyloseq-class object using IR and IS prevalence filtration
+keepTaxa.T2D.fil <- c(keepTaxa.IR, keepTaxa.IS) 
+T2D.fil #1281 taxa 
+T2D.fil <- prune_taxa(keepTaxa.T2D.fil, T2D.fil)
+T2D.fil #1070 taxa
+
+
+#### Create a contingency table of the number of taxa in each phylum for the IR, IS and general T2D 
+### IR
+table(tax_table(T2D)[, "Phylum"]) #3 phylum (Acidobacteria, Fusobacteria and Synergistetes) showed count of only 1
+
+## Compute prevalence of every feature(/taxa?)
+prevalence.df = apply(X = otu_table(T2D),
+                      MARGIN = ifelse(taxa_are_rows(T2D), yes = 1, no = 2),
+                      FUN = function(x){sum(x > 0)})
+
+## add taxonomy and total read counts
+prevalence.df = data.frame(Prevalence = prevalence.df,
+                           TotalAbundance = taxa_sums(T2D),
+                           tax_table(T2D))
+plyr::ddply(prevalence.df, "Phylum", function(df1){cbind(mean(df1$Prevalence),sum(df1$Prevalence))}) #column 1 = mean prevalence, column 2 = prevalence sum
+
+### IS
+
+
+### T2D
+
+
+#### PCoA comparing IR and IS
+
+
+#### PCoA of IR for each race 
+
+
+#### PCoA of IS for each race
+
+
+###########################################################################################################################
+#### Removal of all samples that are not shared between the T2D.rm.gut and the metabolome data
+# NOTE: T2D.rm.gut has 869 samples and the metabolome data has 726 (BEFORE REMOVING ANY SUBJECTS NOT IN THE PHYLOSEQ OBJECT)
+### Load metabolome data
+
+
+#### Multitable analysis 
 
