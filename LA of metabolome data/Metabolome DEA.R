f10d76c12e274d3a2581cead7a16ada9ac571d13
sabdeolive <60741615+sabdeolive@users.noreply.github.com>
2020-05-16 15:31:41 +0200

    Update Metabolome DEA.R

--- LA of metabolome data/Metabolome DEA.R
+++ LA of metabolome data/Metabolome DEA.R
@@ -50,3 +50,56 @@ fit<- eBayes(fit)
 #Summarize results
 results<- decideTests(fit[,"IR_IS_classificationIS"])
 summary(results)
+
+library(ggplot2)
+qplot(colSums(metabolome))
+##############################################################################
+#### With log transformation
+metabolome.log <- log10(metabolome + 1)
+class(metabolome.log)
+dim(metabolome.log)
+metabolomeMat.log <- data.matrix(metabolome.log)
+dim(metabolomeMat.log)
+class(metabolomeMat.log)
+
+#Assigning subject data to subject
+subject <-Subject.data.T2DM.iHMP.5
+class(subject)
+dim(subject)
+
+#Load package
+library(Biobase)
+
+#Creating ExpressionSet object
+eset.log <- ExpressionSet(assayData = metabolomeMat.log,
+                      phenoData = AnnotatedDataFrame(subject))
+# Error in validObject(.Object) : 
+#   invalid class "ExpressionSet" object: 1: sampleNames differ between assayData and phenoData
+# invalid class "ExpressionSet" object: 2: sampleNames differ between phenoData and protocolData
+
+dim(eset.log)
+
+#Install limma
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("limma")
+
+#Translate statistical model to R code (creation of design matrix)
+design<- model.matrix(~IR_IS_classification, data = pData(eset))
+head(design,2)
+colSums(design)
+
+#Load limma
+library(limma)
+
+#Fit the model with lmFit 
+fit.log<- lmFit(eset.log,design)
+
+#Calculate the t-statistics 
+fit.log<- eBayes(fit.log)
+
+#Summarize results
+results.log<- decideTests(fit.log[,"IR_IS_classificationIS"])
+summary(results.log)
+
